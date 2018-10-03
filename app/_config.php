2e8ac32741ef51d99d955f58ea9da1fc4e4ce928
Leo Chen <leo@saltedherring.com>
2018-10-02 19:23:36 -1200

    :/

--- app/_config.php
+++ app/_config.php
@@ -4,7 +4,8 @@ use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 
 // remove PasswordValidator for SilverStripe 5.0
-$validator = PasswordValidator::create();
-$validator->setMinLength(8);
-$validator->setHistoricCount(6);
+$validator = new PasswordValidator();
+
+$validator->minLength(8);
+$validator->checkHistoricalPasswords(6);
 Member::set_password_validator($validator);
