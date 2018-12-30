92bc93e2af1de80e9505c4edd49c6a9c18930aa8
Leo Chen <leochenftw@users.noreply.github.com>
2018-12-30 21:55:44 +1300

    Finally... ss3 style debugger

--- app/src/lib/Debugger.php
+++ app/src/lib/Debugger.php
@@ -6,13 +6,13 @@
  * */
 namespace Leochenftw;
 
+use SilverStripe\Dev\Debug;
+
 class Debugger
 {
     public static function inspect($obj, $die = true)
     {
-        print '<pre>';
-        print_r($obj);
-        print '</pre>';
+        Debug::dump($obj);
         if ($die) {
             die;
         }
@@ -21,9 +21,7 @@ class Debugger
     public static function methods(&$obj)
     {
         if (!empty($obj)) {
-            echo '<pre>';
-            print_r(get_class_methods($obj));
-            echo '</pre>';
+            Debug::dump(get_class_methods($obj));
         } else {
             echo 'object is empty';
         }
