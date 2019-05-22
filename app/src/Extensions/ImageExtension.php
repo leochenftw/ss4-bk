76d0e91b6db1d6bbdba046ba6fc746d5110f1c34
Leo Chen <leochenftw@users.noreply.github.com>
2019-05-23 01:02:10 +1200

    Update ImageExtension.php

--- app/src/Extensions/ImageExtension.php
+++ app/src/Extensions/ImageExtension.php
@@ -8,6 +8,7 @@ class ImageExtension extends DataExtension
 {
     public function getData($resample = 'ScaleWidth', $width = 600, $height = null)
     {
+        if (!$this->owner->exists()) return null;
         return [
             'id'    =>  $this->owner->ID,
             'title' =>  $this->owner->Title,
